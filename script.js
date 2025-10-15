866972bdbf2076ef0fd65c645eb44719d6e642d7
Delete54321 <timjstuder@gmail.com>
2025-10-15 16:02:23 -0400

    version 2

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
+function shuffle(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]]; // Swap elements
+  }
+}
+
+
 function generateLinks() {
     // Get the container element where links will be added
     const container = document.getElementById("fileLinks");
-
+    const link_list = [];
     // Iterate through each file name in the JSON data
     fileNames.files.forEach(fileName => {
         // Create an anchor element for each file
@@ -26,16 +34,23 @@ function generateLinks() {
         // link.appendChild(gameName);
 
         // Append the link to the container
-        list.appendChild(link)
+        list.appendChild(link);
+        link_list.push(list);
         container.appendChild(list);
         
     });
-
+    /*
+    shuffle(link_list);
+    link_list.forEach(li_element => {
+        container.appendChild(li_element);
+    })
+    */
     // Set up navigation event listeners after links are created
     setupNavigation();
 }
 
 function setupNavigation() {
+    const li_elements=document.querySelectorAll('li')
     const elements = document.querySelectorAll('a');
     console.log(elements);
     const gameimage = document.getElementById("gameimage");
@@ -44,11 +59,12 @@ function setupNavigation() {
     if (elements.length > 0) {
         // Initial selection
         elements[currentIndex].classList.add('selected');
+        li_elements[currentIndex].classList.add('selected')
         gameimage.src=elements[currentIndex].getAttribute("thumbnail");
         function navigate(event) {
             // Remove current selection
             elements[currentIndex].classList.remove('selected');
-
+            li_elements[currentIndex].classList.remove('selected')
             /* if (event.key === 'ArrowRight' || event.key==='d') {
                 currentIndex = (currentIndex + 1) % elements.length;
             } else if ( event.key === 'ArrowLeft' || event.key==='a') {
@@ -76,6 +92,7 @@ function setupNavigation() {
 
             // Add new selection
             elements[currentIndex].classList.add('selected');
+            li_elements[currentIndex].classList.add('selected')
             gameimage.src=elements[currentIndex].getAttribute("thumbnail");
 
             /* 
