7dc953402d5f3a9a29c17d9268d8530893ca8b79
Delete54321 <timjstuder@gmail.com>
2025-11-01 11:31:17 -0400

    added try except blocks to most of the important functions

--- pixelpad_importer.py
+++ pixelpad_importer.py
@@ -3,27 +3,49 @@ import os
 import shutil
 
 #gets location of games and places the path in the games_dir variable
-with open('./Games/config.json','r') as f:
-    paths = json.load(f)
-games_dir=paths.get('output_path','')
-f.close()
+try:
+    with open('./Games/config.json','r') as f:
+        paths = json.load(f)
+    games_dir=paths.get('output_path','')
+    f.close()
+except:
+        print("Couldn't load config")
+        input()
+        exit()
+
 
 #gets first file that ends in '.txt' in games director
-game_file=""
+txt_file=""
 for file in os.listdir(games_dir):
     if file.endswith(".txt"):
-        game_file=file
+        txt_file=file
         break
 game_name=""
 game_link=""
-if game_file!="":
-    with open(games_dir+game_file,"r") as f:
-        game_name=f.readline().strip()
-        game_link=f.readline().strip()
+if txt_file!="":
+    with open(games_dir+txt_file,"r") as f:
+        try:
+            game_name=f.readline().strip()
+            game_link=f.readline().strip()
+        except:
+            print("missing game name or link in textfile")
+            input()
+            exit()
     f.close()
-    os.remove(games_dir+game_file)
+    if game_name=="":
+        print("missing game name")
+        input()
+        exit()
+    if game_link=="":
+        print("missing game link")
+        input()
+        exit()
     print(game_name)
     print(game_link)
+else:
+    print("missing text file")
+    input()
+    exit()
 
 
 old_game_thumbnail=''
@@ -33,19 +55,20 @@ for file in os.listdir(games_dir):
         break
 if old_game_thumbnail=="":
     print("no thumbnail image detected")
+    input()
     exit()
 print("old game thumbnail",old_game_thumbnail)
 new_thumbnail=game_name + old_game_thumbnail[-4:]
 print("new game thumbnail",new_thumbnail)
-os.rename(games_dir + old_game_thumbnail , games_dir + new_thumbnail)
-shutil.move(games_dir+new_thumbnail,"./Games/GameThumbnails/")
-
 
 category = "PixelPad"
-
-with open("games.js",'r') as f:
-    gamelist=f.read()
-
+try:
+    with open("games.js",'r') as f:
+        gamelist=f.read()
+except:
+    print("missing games.js")
+    input()
+    exit()
 game_to_be_added=""
 game_to_be_added+='        {\n'
 game_to_be_added+='            "name": "'+game_name+'",\n'
@@ -55,9 +78,31 @@ game_to_be_added+='            "category":"'+category+'",\n'
 game_to_be_added+='        },\n'
 game_to_be_added+=']'
 
-gamelist=gamelist.replace("]",game_to_be_added)
 
+try:
+    os.rename(games_dir + old_game_thumbnail , games_dir + new_thumbnail)
+except:
+    print("couldn't rename image")
+    input()
+    exit()
+try:
+    shutil.move(games_dir+new_thumbnail,"./Games/GameThumbnails/")
+except:
+    print("couldn't move thumbnail image from input directory to GameThumbnails folder")
+    input()
+    exit()
+
+try:
+    os.remove(games_dir+txt_file)
+except:
+    print("could not remove txt file")
+
+try:
+    gamelist=gamelist.replace("]",game_to_be_added)
+except:
+    print("missing ] in games.js")
+    input()
+    exit()
 with open("games.js",'w') as f:
     f.write(gamelist)
 f.close()
-
